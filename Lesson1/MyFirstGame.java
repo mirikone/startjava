f3b185cce312c759fecd11b2b47bd2d6307526a6
mirikone <tagiev.mirabbas@mail.ru>
2021-11-13 13:11:42 +0300

    Lesson1

--- Lesson1/MyFirstGame.java
+++ Lesson1/MyFirstGame.java
@@ -4,10 +4,10 @@ public class MyFirstGame {
         int randomNumber = 60;
         for (int i = 1; i <= 100; i++) {
             if (i == randomNumber) {
-            System.out.println("Поздравляю, число угадано");
-            break;
+                System.out.println("Поздравляю, число угадано");
+                break;
             } else if (i > randomNumber) {
-                        System.out.println("Данной число больше того, что загадал компьютер");
+                System.out.println("Данной число больше того, что загадал компьютер");
             } else {
                 System.out.println("Данной число меньше того, что загадал компьютер");
             }
