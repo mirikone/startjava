8929b35388fd81f86c737047e01d74a46ea7cb4b
mirikone <tagiev.mirabbas@mail.ru>
2021-11-28 20:00:07 +0300

    Lesson2

--- Lesson2/GuessNumber.java
+++ Lesson2/GuessNumber.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class GuessNumber {
-    private boolean isWin;
     private int randomNumber;
     private Player firstPlayer;
     private Player secondPlayer;
@@ -14,33 +13,30 @@ public class GuessNumber {
     }
 
     public void start() {
-        isWin = false;
         Random secretNumber = new Random();
-        randomNumber = secretNumber.nextInt(100);
-        while(!isWin) {
+        randomNumber = secretNumber.nextInt(101);
+        while((!compareNumbers(firstPlayer) || !compareNumbers(secondPlayer)) == false) {
             inputNumber(firstPlayer);
-            playAlmost(firstPlayer);
-            if(isWin == true) {
+            if (compareNumbers(firstPlayer) == false) {
                 break;
             }
             inputNumber(secondPlayer);
-            playAlmost(secondPlayer);
         }
     }
 
-    public void inputNumber(Player player) {
-            System.out.print(player.getName() + " - Введите угадываемое число: ");
-            player.setNumber(scanner.nextInt());
-    }
-
-    public void playAlmost(Player player) {
+    private void inputNumber(Player player) {
+        System.out.print(player.getName() + " - Введите угадываемое число: ");
+        player.setNumber(scanner.nextInt());
         if (player.getNumber() == randomNumber) {
             System.out.println(player.getName() + "- Поздравляю, число угадано");
-            isWin = true;
         } else if (player.getNumber() < randomNumber) {
             System.out.println("Данной число меньше того, что загадал компьютер");
         } else {
             System.out.println("Данной число больше того, что загадал компьютер");
         }
+    }
+
+    private boolean compareNumbers(Player player) {
+        return player.getNumber() != randomNumber;
     }
 }
