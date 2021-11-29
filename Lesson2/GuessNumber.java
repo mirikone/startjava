89f6204fc1789113b49da3c0f05969e7f12a2f38
mirikone <tagiev.mirabbas@mail.ru>
2021-11-29 15:50:25 +0300

    Lesson2

--- Lesson2/GuessNumber.java
+++ Lesson2/GuessNumber.java
@@ -15,28 +15,34 @@ public class GuessNumber {
     public void start() {
         Random secretNumber = new Random();
         randomNumber = secretNumber.nextInt(101);
-        while((!compareNumbers(firstPlayer) || !compareNumbers(secondPlayer)) == false) {
+        System.out.println(randomNumber);
+        do {
             inputNumber(firstPlayer);
-            if (compareNumbers(firstPlayer) == false) {
+            if(compareNumbers(firstPlayer) == false) {
                 break;
             }
             inputNumber(secondPlayer);
-        }
+            if(compareNumbers(secondPlayer) == false) {
+                break;
+            }
+        } while(true);
     }
 
     private void inputNumber(Player player) {
         System.out.print(player.getName() + " - Введите угадываемое число: ");
         player.setNumber(scanner.nextInt());
-        if (player.getNumber() == randomNumber) {
-            System.out.println(player.getName() + "- Поздравляю, число угадано");
-        } else if (player.getNumber() < randomNumber) {
-            System.out.println("Данной число меньше того, что загадал компьютер");
-        } else {
-            System.out.println("Данной число больше того, что загадал компьютер");
-        }
     }
 
-    private boolean compareNumbers(Player player) {
-        return player.getNumber() != randomNumber;
+    private Boolean compareNumbers(Player player) {
+        if (player.getNumber() == randomNumber) {
+            System.out.println(player.getName() + "- Поздравляю, число угадано");
+            return false;
+        } else if (player.getNumber() < randomNumber) {
+            System.out.println("Данной число меньше того, что загадал компьютер");
+            return true;
+        } else {
+            System.out.println("Данной число больше того, что загадал компьютер");
+            return true;
+        }
     }
 }
